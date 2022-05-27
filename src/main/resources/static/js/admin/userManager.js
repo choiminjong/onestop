666848c5a95e077196e182e4141e47ef58ea5e28
민종 최 <dominic@nexon.co.kr>
2022-05-27 10:21:35 +0900

    CLIP 사용자 템플릿 정리

--- src/main/resources/static/js/admin/userManager.js
+++ src/main/resources/static/js/admin/userManager.js
@@ -1,4 +1,33 @@
 
+async function joinForm(){
+    /*
+    Users 회원가입
+    */
+    let data ={
+        username : document.querySelector('#username').value,
+        password : document.querySelector('#password').value,
+        email : document.querySelector('#email').value
+    }
+
+    let url = "/auth/join"
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    });
+
+    let error  = await response.json();
+
+    if(error['status'] == '200'){
+        alert("회원가입이 완료되었습니다.");
+        location.href="/clip/accounts";
+    }else{
+        alert("회원가입을 실패했습니다.");
+    }
+}
+
 async function updateUser() {
     /*
        updateUser 회원 정보 수정
